a36bf2ee8f9fb1f3dce7b41f6a55dd3a80eff0ec
Hakkovacs <89679747+Hakkovacs@users.noreply.github.com>
2023-10-08 07:11:11 +0000

    Update

--- third.py
+++ third.py
@@ -1,6 +1,7 @@
 #Excercise 3 Find the largest number
 import sys
 largest_number = 0
+all_numbers = []
 
 count = int (input ("How many numbers do you want to compare? "))
 if count <= 0:
@@ -9,9 +10,10 @@ if count <= 0:
 
 for i in range(count):
   number = int (input (f'Enter number {i+1}: \n'))
+  all_numbers.append (number)
   if i == 0 :
     largest_number = number
   if number > largest_number :
     largest_number = number
 
-print (f"The largest number is {largest_number} \n")
+print (f"Your number set consists of {all_numbers} and the largest number is {largest_number} \n")
