57dbe97ba44829e5a12c11899ea94be81b14ec4f
Hakkovacs <89679747+Hakkovacs@users.noreply.github.com>
2023-10-10 13:27:58 +0000

    Search added

--- bsort.py
+++ bsort.py
@@ -1,6 +1,3 @@
-# Optimized Python program for implementation of Bubble Sort
-
-
 def bubbleSort(arr):
 	n = len(arr)
 	
@@ -8,12 +5,7 @@ def bubbleSort(arr):
 	for i in range(n):
 		swapped = False
 
-		# Last i elements are already in place
 		for j in range(0, n-i-1):
-
-			# Traverse the array from 0 to n-i-1
-			# Swap if the element found is greater
-			# than the next element
 			if arr[j] > arr[j+1]:
 				arr[j], arr[j+1] = arr[j+1], arr[j]
 				swapped = True
