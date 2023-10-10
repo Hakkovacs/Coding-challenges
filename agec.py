42b0502f7896cbaf6d4a1607108896faed68a411
Hakkovacs <89679747+Hakkovacs@users.noreply.github.com>
2023-10-10 18:15:18 +0000

    Stage new

---
+++ agec.py
@@ -0,0 +1,26 @@
+import datetime
+
+date_format = "%Y-%m-%d"
+current_date = datetime.date.today()
+current_year = datetime.date.today().year
+min_date = datetime.date(1, 1, 1)
+#current_date_string = current_date.strftime(date_format)
+
+birth_year = int (input('Enter the year you were born?\n'))
+print (f"You are {current_year - birth_year} old \n")
+
+birth_date_input = (input('Enter your the date of your birth (yyyy-mm-dd): \n'))
+birth_date = datetime.datetime.strptime(birth_date_input, date_format).date()
+
+if birth_date > min_date and birth_date < current_date :
+  birthday_this_year = birth_date.replace (year=current_date.year)
+
+  if birthday_this_year > current_date :
+    count_days = birthday_this_year - current_date
+  else :
+    birthday_this_year = birth_date.replace (year=current_date.year+1)
+
+  print (f"You have {birthday_this_year - current_date} days until your birthday")
+else :
+  print ("You entered wrong date")
+
--- search.py
+++ search.py
@@ -5,35 +5,36 @@
 # It returns location of x in given array arr
 def binarySearch(arr, l, r, x):
 
-	while l <= r:
+  while l <= r:
 
-		mid = l + (r - l) // 2
+    mid = l + (r - l) // 2
+    print (f"left: {l}, middle: {mid}, right {r}, search for {x}")
 
-		# Check if x is present at mid
-		if arr[mid] == x:
-			return mid
+  # Check if x is present at mid
+    if arr[mid] == x:
+      return mid
 
-		# If x is greater, ignore left half
-		elif arr[mid] < x:
-			l = mid + 1
+  # If x is greater, ignore left half
+    elif arr[mid] < x:
+      l = mid + 1
 
-		# If x is smaller, ignore right half
-		else:
-			r = mid - 1
+  # If x is smaller, ignore right half
+    else:
+      r = mid - 1
 
-	# If we reach here, then the element
-	# was not present
-	return -1
+  # If we reach here, then the element
+  # was not present
+  return -1
 
 
 # Driver Code
 if __name__ == '__main__':
-	arr = [2, 3, 4, 10, 40]
-	x = 10
+  arr = [1, 2, 3, 4, 10, 40, 50, 60]
+  x = 10
 
-	# Function call
-	result = binarySearch(arr, 0, len(arr)-1, x)
-	if result != -1:
-		print("Element is present at index", result)
-	else:
-		print("Element is not present in array")
+  # Function call
+  result = binarySearch(arr, 0, len(arr)-1, x)
+  if result != -1:
+    print("Element is present at index", result)
+  else:
+    print("Element is not present in array")
